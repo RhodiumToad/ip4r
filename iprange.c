9e8ff7cbd1b9ebafb10cf67273f0f6733fb4238d
Peter Eisentraut <peter@eisentraut.org>
2016-04-23 22:09:25 -0400

    Simplify code
    The "else if" is redundant with the "if", and somehow it confuses clang
    into thinking that retval may go uninitialized, so just simplify the
    code and get rid of the warnings.

--- iprange.c
+++ iprange.c
@@ -1112,7 +1112,7 @@ iprange_overlaps_internal(Datum d1, Datum d2)
 				iprange_internal_error();
 		}
 	}
-	else if (af1 != af2)
+	else
 		retval = (af1 == 0) || (af2 == 0);
 
 	if ((Pointer)ipp1 != DatumGetPointer(d1))
@@ -1154,7 +1154,7 @@ iprange_contains_internal(Datum d1, Datum d2, bool eqval)
 				iprange_internal_error();
 		}
 	}
-	else if (af1 != af2)
+	else
 		retval = (af1 == 0);
 
 	if ((Pointer)ipp1 != DatumGetPointer(d1))
@@ -1193,7 +1193,7 @@ iprange_contains_ip_internal(Datum d1, int af2, IP4 ip4, IP6 *ip6)
 				iprange_internal_error();
 		}
 	}
-	else if (af1 != af2)
+	else
 		retval = (af1 == 0);
 
 	if ((Pointer)ipp1 != DatumGetPointer(d1))
