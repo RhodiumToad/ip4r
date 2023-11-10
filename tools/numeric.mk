1a3025d6d5b5cc4ad021a7607d32285a3378e630
liuxueyang <liuxueyang457@gmail.com>
2023-11-10 11:30:52 +0800

    Remove the newline for the call function
    If we add the newline in call arguments. Then the newline and leading
    whitespaces will be treated as part of the arguments. The `words` and
    `firstword` functions in GNU make 3.82 will treat newline as word separator
    while GNU make 4.3 won't. This results the function "nwords` will report error
    like: "first argument to `word' function must be greater than 0.  Stop."

--- tools/numeric.mk
+++ tools/numeric.mk
@@ -15,9 +15,7 @@ $(if $(strip $(1)),
                t,
                $(if $(call numeric_lt,$(firstword $(1)),$(firstword $(2))),
                     ,
-                    $(call vge_2,
-                           $(wordlist 2,$(words $(1)),$(1)),
-                           $(wordlist 2,$(words $(2)),$(2))))),
+                    $(call vge_2,$(wordlist 2,$(words $(1)),$(1)),$(wordlist 2,$(words $(2)),$(2))))),
           t),
      $(if $(strip $(2)),,t))
 endef
@@ -30,6 +28,22 @@ numeric_lt = $(if $(call numeric_le,$(2),$(1)),,t)
 
 # list of N words
 nwords = $(if $(filter-out 0,$(1)),$(if $(word $(1),$(2)),$(2),$(call nwords,$(1),$(2) t)))
+
+define assert
+$(if $1,,$(error assert failed: $2))
+endef
+
+define assert_n
+$(if $1,$(error assert_n failed: $2),)
+endef
+
+.PHONY: test_tools_numeric
+
+tools_test_numeric:
+    $(call assert_n,$(call version_ge,9.0,9.1))
+    $(call assert,$(call version_ge,9.2,9.1))
+    $(call assert,$(call version_ge,9.0,9.0))
+    $(call assert_n,$(call version_ge,9.4,13))
 
 ## end of hacks.
 ##--
